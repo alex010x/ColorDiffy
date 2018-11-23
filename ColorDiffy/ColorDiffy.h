606cb76c0b0d462d2c8513f4789070c45966d89f
Alessandro Minopoli <minopolialessandro07@gmail.com>
2018-11-23 20:25:04 +0100

    Initial Commit

---
+++ ColorDiffy/ColorDiffy.h
@@ -0,0 +1,19 @@
+//
+//  ColorDiffy.h
+//  ColorDiffy
+//
+//  Created by Alessandro Minopoli on 23/11/2018.
+//  Copyright © 2018 Alessandro Minopoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorDiffy.
+FOUNDATION_EXPORT double ColorDiffyVersionNumber;
+
+//! Project version string for ColorDiffy.
+FOUNDATION_EXPORT const unsigned char ColorDiffyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorDiffy/PublicHeader.h>
+
+
