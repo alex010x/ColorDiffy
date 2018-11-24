be81c0e6e640fac88138b41fb40ac01ed1079ee2
Alessandro Minopoli <minopolialessandro07@gmail.com>
2018-11-24 16:20:42 +0100

    fix differences check.

--- ColorDiffy/ColorDiffy.swift
+++ ColorDiffy/ColorDiffy.swift
@@ -13,36 +13,48 @@ public class ColorDiffy {
     /// - parameters:
     ///   - stringOne: the first String to be compared.
     ///   - stringTwo: the second String to be compared. This will be the colorized String.
-    ///   - options: type of String Enumeration: can be .byWords, .bySentence, .byLines, etc. Default value is .byWords.
     ///   - color: the associated color to use to highlight the differences. Default color is red.
     ///   - returns: an NSMutableAttributedString object.
 
-    public class func findDifferencesBetween(stringOne: String, stringTwo: String, options: String.EnumerationOptions = .byWords, color: UIColor = .red) -> NSMutableAttributedString {
-        
+    public class func findDifferencesBetween(stringOne: String, stringTwo: String, color: UIColor = .red) -> NSMutableAttributedString {
+
         var rangesToConvert: [NSRange] = []
         var componentsNew = [(str: String, range: NSRange)]()
         var componentsOld = [(str: String, range: NSRange)]()
+        var evaluatedIndexes = Set<Int>()
         let mutableString = NSMutableAttributedString(string: stringTwo)
-        stringTwo.enumerateSubstrings(in: stringTwo.startIndex..<stringTwo.endIndex, options: options) { substring, substringRange, enclosingRange, stop in
+        stringTwo.enumerateSubstrings(in: stringTwo.startIndex..<stringTwo.endIndex, options: .byWords) { substring, substringRange, enclosingRange, stop in
             componentsNew.append((str: substring!, range: NSRange(substringRange, in: stringTwo)))
         }
-        stringOne.enumerateSubstrings(in: stringOne.startIndex..<stringOne.endIndex, options: options) { substring, substringRange, enclosingRange, stop in
+        stringOne.enumerateSubstrings(in: stringOne.startIndex..<stringOne.endIndex, options: .byWords) { substring, substringRange, enclosingRange, stop in
             componentsOld.append((str: substring!, range: NSRange(substringRange, in: stringOne)))
         }
         var s = 0
+        var invalidJump: Bool = false
         for i in 0..<componentsOld.count {
+            invalidJump = false
             if i>=componentsNew.count { break }
-            if s<i {
-                s=i
-            }
             while s<componentsNew.count && componentsNew[s].str != componentsOld[i].str {
                 rangesToConvert.append(componentsNew[s].range)
+                evaluatedIndexes.insert(s)
+                s+=1
+                if i+1 < componentsOld.count && s < componentsNew.count {
+                    if componentsNew[s].str == componentsOld[i+1].str {
+                        invalidJump = true
+                        break
+                    }
+                }
+            }
+            if !invalidJump {
+                evaluatedIndexes.insert(s)
                 s+=1
             }
         }
         if componentsNew.count > componentsOld.count {
             for i in componentsOld.count..<componentsNew.count {
-                rangesToConvert.append(componentsNew[i].range)
+                if !evaluatedIndexes.contains(i) {
+                    rangesToConvert.append(componentsNew[i].range)
+                }
             }
         }
         for range in rangesToConvert {
@@ -51,4 +63,5 @@ public class ColorDiffy {
         return mutableString
     }
 }
+
 
